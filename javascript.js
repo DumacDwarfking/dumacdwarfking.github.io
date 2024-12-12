55adecf7ce556d852c2de384289e5ddaf9b6b0fa
Kasper Futyna <kasfutable@gmail.com>
2024-12-12 19:10:45 +0100

    Added working Fetch to website JS file

--- javascript.js
+++ javascript.js
@@ -1,11 +1,23 @@
-let url = "https://api.nasa.gov/planetary/apod";
+//Select relevant elements:
+var title = document.querySelector(".title");
+var date = document.querySelector(".date");
+var description = document.querySelector(".description");
+var spaceImage = document.getElementById("spaceImage");
+var copyright = document.querySelector(".copyright");
+
+var APIkey = "K8jf2EtMCnVq9g4aVD01td9nkigsstb4VvBTdV4b";
+var url = "https://api.nasa.gov/planetary/apod?api_key=" + APIkey;
 
 fetch(url)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Could not fetch resource!");
-    }
-    return response.json();
+  .then(function (response) {
+    return response.json(); //Parse response into JSON
   })
-  .then((data) => console.log(data.name))
-  .catch((error) => console.error(error));
+  .then(function (data) {
+    title.textContent = data.title;
+    description.textContent = data.explanation;
+    spaceImage.src = data.url;
+    console.log(data.url);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
