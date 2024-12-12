42fb54643ef186533b117c9fb1b80b8d0ea4fde3
Kasper Futyna <kasfutable@gmail.com>
2024-12-12 20:59:33 +0100

    Added diplay Button and Function

--- javascript.js
+++ javascript.js
@@ -1,23 +1,37 @@
-//Select relevant elements:
-var title = document.querySelector(".title");
-var date = document.querySelector(".date");
-var description = document.querySelector(".description");
-var spaceImage = document.getElementById("spaceImage");
-var copyright = document.querySelector(".copyright");
+var fetcher = document.querySelector(".fetcher");
+var container = document.querySelector(".container");
 
-var APIkey = "K8jf2EtMCnVq9g4aVD01td9nkigsstb4VvBTdV4b";
-var url = "https://api.nasa.gov/planetary/apod?api_key=" + APIkey;
+//Fetch on button click, display elements:
+fetcher.addEventListener("click", () => {
+  fetcher.classList.toggle("active");
+  container.classList.toggle("active");
 
-fetch(url)
-  .then(function (response) {
-    return response.json(); //Parse response into JSON
-  })
-  .then(function (data) {
-    title.textContent = data.title;
-    description.textContent = data.explanation;
-    spaceImage.src = data.url;
-    console.log(data.url);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+  var APIkey = "K8jf2EtMCnVq9g4aVD01td9nkigsstb4VvBTdV4b";
+  var url = "https://api.nasa.gov/planetary/apod?api_key=" + APIkey;
+
+  fetch(url)
+    .then(function (response) {
+      //Parse response into JSON
+      return response.json();
+    })
+    .then(function (data) {
+      //Select relevant elements:
+      var title = document.querySelector(".title");
+      var date = document.querySelector(".date");
+      var description = document.querySelector(".description");
+      var spaceImage = document.getElementById("spaceImage");
+      var copyright = document.querySelector(".copyright");
+
+      //Add text and image to elements
+      spaceImage.src = data.url;
+      title.textContent = data.title;
+      date.textContent = data.date;
+      description.textContent = data.explanation;
+
+      let copyrightName = data.copyright;
+      copyright.textContent = "© " + copyrightName;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
